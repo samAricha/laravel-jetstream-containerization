6b9f780adfeddf89e873243b836cfd375adf4901
samAricha <samaricha001@gmail.com>
2023-08-14 23:52:59 +0300

    adding content security to app.blade.php file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
